ac67e817238ac13a06eacc62755e01ee1f3aa3dd
beautifulseoul <1991scarlett@gmail.com>
2022-11-16 11:05:44 +0900

    수정사항 반영

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 from pymongo import MongoClient
 import certifi
 
-ca=certifi.where()
+ca = certifi.where()
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.shbwsw1.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
@@ -18,17 +18,18 @@ import datetime
 
 import hashlib
 
+
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.user.find_one({"id": payload['id']})
-        return render_template('index.html', nickname=user_info["nick"])
+        user_info = db.wishtree.find_one({"id": payload['id']})
+        return render_template('tree.html', nickname=user_info["nick"])
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+        return  redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 
 @app.route('/login')
@@ -40,7 +41,6 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
-
 
 #################################
 ##  로그인을 위한 API            ##
@@ -61,6 +61,7 @@ def api_register():
 
     return jsonify({'result': 'success'})
 
+
 @app.route('/api/login', methods=['POST'])
 def api_login():
     id_receive = request.form['id_give']
@@ -69,8 +70,8 @@ def api_login():
     pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
 
     result = db.wishtree.find_one({'id': id_receive, 'pw': pw_hash})
+    print(result)
     # 여기까지 확인함 TTTT
-
 
     # 찾으면 JWT 토큰을 만들어 발급합니다.
     if result is not None:
@@ -80,10 +81,9 @@ def api_login():
         # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
         payload = {
             'id': id_receive,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=20)  #utc timezone기준으로 now
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=120)  # utc timezone기준으로 now
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-        print(token)
         # token을 줍니다.
         return jsonify({'result': 'success', 'token': token})
     # 찾지 못하면
@@ -105,7 +105,7 @@ def api_valid():
     try:
         # token을 시크릿키로 디코딩합니다.
         # 보실 수 있도록 payload를 print 해두었습니다. 우리가 로그인 시 넣은 그 payload와 같은 것이 나옵니다.
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])    #jwt.decode는 jwt페이지에서 보는 decode와 같음
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])  # jwt.decode는 jwt페이지에서 보는 decode와 같음
         print(payload)
 
         # payload 안에 id가 들어있습니다. 이 id로 유저정보를 찾습니다.
@@ -117,6 +117,49 @@ def api_valid():
         return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
     except jwt.exceptions.DecodeError:
         return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
+
+@app.route("/wish", methods=["POST"])
+def save_wish():
+    wish_receive = request.form['wish_give']
+    id_redeive = request.form['id_give']
+
+    wish_list = list(db.wish.find({}, {'_id': False}))
+    count = len(wish_list) + 1
+
+    doc = {
+        'wish': wish_receive,
+        'num': count,
+        'id': id_redeive,
+        'done': 0
+    }
+    db.wish.insert_one(doc)
+    return jsonify({'msg': '소원빌기 완료'})
+
+@app.route("/wish", methods=["GET"])
+def wish_get():
+    wish_list = list(db.wish.find({}, {'_id': False}))
+    return jsonify({'wishes': wish_list})
+
+@app.route("/wish/done", methods=["POST"])
+def wish_done():
+    num_receive = request.form['num_give']
+    db.wish.update_one({'num': int(num_receive)}, {'$set':{'done':1}})
+    return jsonify({'msg': '소원성취!!'})
+
+
+
+@app.route("/lounge/get", methods=["GET"])
+def set_temp():
+    user_list = list(db.wishtree.find({}, {'_id': False}))
+    return jsonify({'users':user_list})
+
+@app.route('/<path>')
+def get_path(path):
+    return render_template(path + '.html')
+
+@app.route('/tree/<id>')
+def get_tree_path(id):
+    return render_template('tree.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
